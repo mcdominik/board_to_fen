dbcf78a24fae9eb5c7bc674a8ea219f465a61659
mcdominik <dominik8881@wp.pl>
2023-01-29 23:11:27 +0100

    debug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
    name='board_to_fen',
-   version='0.0.18',
+   version='0.0.19',
    author='Dominik Maćkiewicz',
    author_email='dominik.mackiewicz@icloud.com',
    packages=find_packages(),
